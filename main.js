401b5ea33252a3c6e666fa9358bfa46fd6b694fd
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-05 23:59:49 +0200

    JS_Exercice_P2_1

---
+++ main.js
@@ -0,0 +1,10 @@
+// fonction qui change l'image au clic avec l'évenemnt (onclick dans le html)
+function changePic() {
+document.getElementById("myImg").src = "./images/image1_2.jpg";
+   }
+  
+
+
+
+
+
